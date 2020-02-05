2e858478690cd035cfeda5a5246fb606b5d0e243
nehaudeshi <udeshineha@yahoo.co.in>
2020-02-05 14:58:22 +0530

    Questionnaire errors solved

--- app/src/main/java/com/example/destressit/Questionnaire.java
+++ app/src/main/java/com/example/destressit/Questionnaire.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -18,6 +19,7 @@ public class Questionnaire extends AppCompatActivity {
     Integer c,d=0;
     RadioGroup rg;
     String SRBtext;
+    int selectedId;
     String[] stringArray = {"Q1","Q2","Q3","Q4","Q5"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,30 +32,39 @@ public class Questionnaire extends AppCompatActivity {
         r3 = findViewById(R.id.radioButton5);
         r4 = findViewById(R.id.radioButton6);
         rg = findViewById(R.id.radioGroup);
+        selectedId = rg.getCheckedRadioButtonId();
+
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                selectedId = rg.getCheckedRadioButtonId();
+            }
+        });
     }
 
     public void onclick(View view){
-            /*t1.setVisibility(View.VISIBLE);*/
-            r1.setChecked(false);
-            r2.setChecked(false);
-            r3.setChecked(false);
-            r4.setChecked(false);
-            t1.setText(stringArray[i]);
-            i=i+1;
 
-            int selectedId = rg.getCheckedRadioButtonId();
-            SRB = (RadioButton) findViewById(selectedId);
-            Toast.makeText(this,SRB.getText().toString(), Toast.LENGTH_SHORT).show();
+        Log.d("CHECK", Integer.toString(selectedId));
 
-            SRBtext = SRB.getText().toString();
-            if(SRBtext == "Most of the times")
-            {
-                c=c+1;
-            }
-            if(SRBtext == "Always")
-            {
-                d=d+1;
-            }
+        SRB = (RadioButton) findViewById(selectedId);
+        Toast.makeText(this,SRB.getText().toString(), Toast.LENGTH_SHORT).show();
+
+        SRBtext = SRB.getText().toString();
+        if(SRBtext == "Most of the times")
+        {
+            c=c+1;
+        }
+        if(SRBtext == "Always")
+        {
+            d=d+1;
+        }
+
+        r1.setChecked(true);
+        r2.setChecked(false);
+        r3.setChecked(false);
+        r4.setChecked(false);
+        t1.setText(stringArray[i]);
+        i=i+1;
 
     }
 
