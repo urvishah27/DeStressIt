324496e9308f1749d83eb261d643896f1dd5f40e
nehaudeshi <udeshineha@yahoo.co.in>
2020-02-07 15:38:27 +0530

    Therapists Portal Complete

---
+++ app/src/main/java/com/example/destressit/ProfileTherapistsFragment.java
@@ -0,0 +1,46 @@
+package com.example.destressit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.destressit.core.PreferenceUtil;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ProfileTherapistsFragment extends Fragment {
+
+    private FirebaseAuth mAuth;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_therapists_profile, container, false);
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
+        getView().findViewById(R.id.edit_therapist_icon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(),EditProfileActivity.class));
+            }
+        });
+        getView().findViewById(R.id.therapist_signOut).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                PreferenceUtil.clearAllPreferences(getContext());
+                startActivity(new Intent(getContext(),RegistrationActivity.class));
+            }
+        });
+    }
+}
