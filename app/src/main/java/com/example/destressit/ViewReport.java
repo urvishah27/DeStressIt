22214060cefe06a75c60a5490025aa7d8e385d2c
Nemilshah1999 <nemilshah1999@gmail.com>
2020-02-08 14:21:06 +0530

    viewReportUser

--- app/src/main/java/com/example/destressit/ViewReport.java
+++ app/src/main/java/com/example/destressit/ViewReport.java
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
+
+import java.util.Observable;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
 public class ViewReport extends AppCompatActivity {
 
@@ -12,6 +17,7 @@ public class ViewReport extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_report);
+
     }
 
     public void onclick(View view){
