1786c4dbdefb5c7881fd5ea2072f398f434bfdef
Nemilshah1999 <nemilshah1999@gmail.com>
2020-02-12 22:24:25 +0530

    layoutIntegration

--- app/src/main/java/com/example/destressit/Recommendation.java
+++ app/src/main/java/com/example/destressit/Recommendation.java
@@ -1,31 +1,35 @@
 package com.example.destressit;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.destressit.core.PreferenceUtil;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.NestedScrollingChild;
+import androidx.core.widget.NestedScrollView;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.View;
+import android.widget.TextView;
 
 public class Recommendation extends AppCompatActivity {
+
+    private Object ViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommendation);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        ViewPager mViewPager = (ViewPager)findViewById(R.id.ViewPager1);
+        ImageAdapter2 adapterView = new ImageAdapter2(this);
+        mViewPager.setAdapter(adapterView);
+
     }
+
 }
